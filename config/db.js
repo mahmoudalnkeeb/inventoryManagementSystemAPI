1744301f89416538e5ce0b14cd6622b88271d6cf
Mahmoud Alnkeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2022-02-19 00:02:54 +0200

    update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+console.log(`${process.env.DB_USER}:${process.env.DB_PASS}`);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvujt.mongodb.net/inventory_management_system?retryWrites=true&w=majority`;
 mongoose.connect(uri, {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -10,8 +10,13 @@ mongoose
 app.use(express.static('./public'))
 app.use(bodyParser.json())
 app.use(routes)
+const dotenv = require('dotenv');
+dotenv.config();
 
-const port = process.env.PORT || 3000
-app.listen(port , ()=>{
-    console.log(`Inventory Management System is now running on http://127.0.0.1:${port}`);
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'http://127.0.0.1';
+
+app.listen(port, () => {
+  console.log(`Inventory Management System is now running on ${host}:${port}`);
+  console.log(`${process.env.DB_USER}:${process.env.DB_PASS}`);
 });
