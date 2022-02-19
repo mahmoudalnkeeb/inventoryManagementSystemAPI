37581c9bea21ec626ab7e8ee964a04ea60d326d7
Mahmoud Alnkeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2022-02-19 19:11:46 +0200

    add request body to logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -4,16 +4,17 @@ const logger = (req, res, next) => {
   if (req.method == 'POST') {
     console.log(
       `Request: 
-         BODY: ${req.body}
-         PATH: ${req.path} 
-         from ip ${JSON.stringify(results)} `
+        BODY: ${req.body}
+        PATH: ${JSON.stringify(req.path)} 
+        IP: from ip ${JSON.stringify(results)} `
     );
     next();
   } else {
     console.log(
-      `Request: ${req.method} ${req.path} from ip ${JSON.stringify(
-        results
-      )} `
+      `Request: 
+        METHOD: ${req.method}
+        PATH: ${req.path}
+        IP: from ip ${JSON.stringify(results)} `
     );
     next();
   }
